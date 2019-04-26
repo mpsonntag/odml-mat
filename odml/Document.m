0c91531e84e04595f92deeb69ec0737374916f32
M. Sonntag <michael.p.sonntag@gmail.com>
2019-04-26 18:00:54 +0200

    Add Document

---
+++ odml/Document.m
@@ -0,0 +1,15 @@
+classdef Document < Sectionable
+    %DOCUMENT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function obj = Document()
+            obj = obj@Sectionable();
+        end
+    end
+    
+end
+
